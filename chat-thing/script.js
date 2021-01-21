6be13aa5a788c246f25f25fa8bc1681054551676
Zagyen8913 <65826867+Zagyen8913@users.noreply.github.com>
2021-01-21 07:31:43 +0100

    Create script.js

---
+++ chat-thing/script.js
@@ -0,0 +1,11 @@
+// zagyen is still learning js so this may take some time
+
+var username;
+
+function updateUsername(newUsername) {
+  username = newUsername;
+}
+
+var logs;
+
+console.log("<div>");
